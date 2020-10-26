844ef51f7398b05bff84dc02592070cab458bc0a
shravani20git <chavanke4shravani@gmail.com>
2020-10-26 11:24:17 +0530

    Second commit

--- database.js
+++ database.js
@@ -5,7 +5,6 @@ var connection = new mysql({
     password: '',
     database: 'inventory'
 });
-
 insert_products = (data) => {
     let sql = "insert into products(name, description, group_id ,measuring_unit) values (?,?,?,?)";
     let sql2 = "insert into price_details(product_id,cost_price,selling_price,mrp,discount) values(?,?,?,?,?)"
@@ -61,8 +60,9 @@ insert_supplier = (data) => {
       return false;
     }
   };
-  
   insert_creditorProfile = (data) => {
+    console.log("creds");
+    console.log(data);
     let sql =
       "insert into creditor_profile(name, address, phone ,total_amt_payable,amt_paid) values (?,?,?,?,?)";
     try {
@@ -79,22 +79,194 @@ insert_supplier = (data) => {
       return false;
     }
   };
-  
 get_groups = () => {
     res = connection.query("select * from product_group")
     return res
 }
-
 get_suppilers = () => {
-    res = connection.query("select * from suppiler")
+    res = connection.query("select * from supplier")
     return res
 }
-
 console.log("conneced to databse")
+get_transactions = (data) => {
+    console.log(data.from_date);
+    console.log(data.to_date);
+    let sql = "select * from transactions where p_date between  ? and ?";
+    console.log("sql query");
+    console.log(sql);
+    try {
+      res = connection.query(sql,[data.from_date, data.to_date]);
+      console.log(res)
+      return res;
+    } catch (err) {
+      console.log("Could't fetch");
+      console.log(err);
+      return null;
+    }
+    
+}
+get_creditors = () =>{
+  res = connection.query("select * from creditor_profile");
+  return res
+}
+
+
+
+del_creditor = (data) =>{
+  console.log(data)
+  console.log("Inside del db");
+   let sql = "DELETE FROM  creditor_profile where id = ?";
+   try{
+     res = connection.query(sql,[data.id]);
+     console.log(res)
+     return true;
+
+   }catch(err){
+     console.log(err)
+     return false;
+   }
+}
+get_creditors_id = (data) =>{
+  res = connection.query("select * from creditor_profile where id = ?",[data.id]);
+  return res
+
+}
+update_creditorProfile = (data) => {
+
+  let sql =
+    "update creditor_profile set name = ?, address = ? , phone = ? ,total_amt_payable= ? , amt_paid= ? where id = ?";
+    try {
+      res = connection.query(sql, [
+        data.name,
+        data.address,
+        data.phone,
+        data.total_amt_payable,
+        data.amt_paid,
+        data.id,
+      ]);
+      return true;
+    } catch (err) {
+      console.log(err);
+      return false;
+    }
+};
+
+update_supplier = (data) =>{
+  let sql = "update supplier set name = ?, address = ? , phone_no = ? ,account_no = ? where id = ?";
+  try {
+    res = connection.query(sql, [
+      data.name,
+      data.address,
+      data.phone_no,
+      data.account_no,
+     
+      data.id,
+    ]);
+    return true;
+  } catch (err) {
+    console.log(err);
+    return false;
+  }
+};
+
+get_products = () =>{
+  res = connection.query("SELECT DISTINCT o.id , o.name,o.group_id,o.description,o.measuring_unit, c.g_name,c.description as gdes,c.total_items , p.cost_price , p.selling_price, p.mrp , p.discount FROM products as O JOIN product_group as c ON O.group_id = c.id Join price_details p WHERE o.id = p.product_id");
+  return res;
+
+}
+get_currentInventory = () =>{
+  res = connection.query("SELECT ci.product_id,  s.name , s.group_id , s.description , s.measuring_unit , pd.cost_price , pd.selling_price , pd.mrp, pd.discount , pg.g_name , pg.description  as gdes , pg.total_items,   ci.quantity from  current_inventory as ci JOIN products as s on s.id = ci.product_id JOIN  price_details as pd on pd.product_id = ci.product_id JOIN product_group as pg WHERE pg.id = s.group_id");
+  return res;
+}
+get_product_by_id = (data) =>{
+  res = connection.query("select * from products where id = ?",[data.id]);
+  return res;
+}
+get_supplier_by_id = (data) =>{
+  res = connection.query("select * from supplier where id = ?",[data.id]);
+  return res;
+}
+del_product = (data) =>{
+  console.log(data)
+  console.log("Inside del product");
+   let sql = "DELETE FROM  products where id = ?";
+   try{
+     res = connection.query(sql,[data.id]);
+     console.log(res)
+     return true;
+
+   }catch(err){
+     console.log(err)
+     return false;
+   }
+}
+get_retailer = (data) =>{
+  console.log("inside get retailer");
+  console.log(data)
+  let sql = "select * from retailer where username = ? and password = ?";
+  try{
+    res = connection.query(sql,[data.username,data.password])
+    console.log("running query");
+    console.log(res.length)
+    if(res.length == 0){
+    return false;
+    }
+    return true;
+  }catch(err){
+    console.log(err)
+    return false;
+  }
+}
+del_supplier= (data) =>{
+  console.log(data)
+  console.log("Inside del supplier");
+   let sql = "DELETE FROM  supplier where id = ?";
+   try{
+     res = connection.query(sql,[data.id]);
+     console.log(res)
+     return true;
+
+   }catch(err){
+     console.log(err)
+     return false;
+   }
+}
+update_product = (data) => {
+//Full texts	id name group_id description measuring_unit 
+console.log("Update")
+console.log(data)
+console.log("Update id ")
+console.log(data.id)
+  let sql =
+    "update products set name = ?, group_id = ? , description = ? ,measuring_unit = ?  where id = ?";
+  let sql2 = " update  price_details set   cost_price = ?,  selling_price=? , mrp=? , discount = ?  where product_id = ?";
+    try {
+      res = connection.query(sql, [
+        data.name,
+        data.group_id,
+        data.description,
+        data.measuring_unit,
+        data.id,
+      ]);
+      console.log(res)
+      console.log("uadte res id")
+      console.log(res.updateId)
+      res2 = connection.query(sql2, [
+       data.cost_price, data.selling_price, data.mrp, data.discount, data.id,
+      ]);
+      console.log(res2)
+      
+      return true;
+    } catch (err) {
+      console.log(err);
+      return false;
+    }
+};
+
+
 
 module.exports = { insert_products, get_groups, get_suppilers, insert_creditorProfile,insert_productGroup,insert_supplier,
-    insert_retailer,
-    
-    
-
+    insert_retailer,get_transactions, get_creditors,del_creditor,get_creditors_id , update_creditorProfile ,
+    update_product, del_product, get_product_by_id ,get_products , del_supplier,update_supplier,get_supplier_by_id,get_retailer,
+    get_currentInventory ,
 };
--- index.js
+++ index.js
@@ -6,9 +6,37 @@ const db = require("./database")
 app.use(bodyParser.urlencoded({ extended: false }));
 app.set("view engine", "ejs");
 app.use(express.static(path.join(__dirname, "public")));
+const  moment = require("moment");
+app.locals.moment = moment;
+app.use(express.static(path.join(__dirname,"public")));
 
 app.get("/", (req, res) => {
     res.render("index")
+})
+app.get("/view_creditors", (req, res) => {
+  creds = db.get_creditors()
+  console.log(creds)
+  res.render("view_creditors",{creditors:creds})
+})
+
+app.get("/view_currentInvent", (req, res) => {
+  invt = db.get_currentInventory()
+  console.log(invt)
+  res.render("view_currentInvent",{current:invt})
+})
+app.get("/view_product", (req, res) => {
+  prods = db.get_products()
+  console.log("getting products")
+  console.log(prods)
+  res.render("view_product",{product:prods,del:null,update:null})
+})
+
+
+app.get("/view_supplier", (req, res) => {
+  sup = db.get_suppilers()
+  console.log("getting supplier")
+  console.log(sup)
+  res.render("view_supplier",{suppliers:sup,del:null,update:null})
 })
 app.get("/insert_product", (req, res) => {
     grp = db.get_groups()
@@ -16,29 +44,41 @@ app.get("/insert_product", (req, res) => {
     res.render("insert_product", { groups: grp, inserted: null })
 })
 app.get("/insert_creditorProfile", (req, res) => {
-    groups = db.get_groups();
-    console.log(groups);
-    res.render("insert_creditorProfile", groups);
+
+    res.render("insert_creditorProfile", {inserted: null});
   });
   app.get("/insert_supplier", (req, res) => {
-    groups = db.get_groups();
-    console.log(groups);
-    res.render("insert_supplier", groups);
+    res.render("insert_supplier", {inserted: null});
   });
   app.get("/insert_retailer", (req, res) => {
-    groups = db.get_groups();
-    console.log(groups);
-    res.render("insert_retailer", groups);
+    res.render("insert_retailer", {inserted: null});
   });
   
   app.get("/insert_productGroup", (req, res) => {
-    groups = db.get_groups();
-    console.log(groups);
-    res.render("insert_productGroup", groups);
+  
+    res.render("insert_productGroup", {inserted: null});
   });
+
+  app.get("/view_transactions", (req, res) => {
+   // trans = db.get_transactions()
+    //console.log(trans)
+  
+    res.render("view_transactions", {transaction: null} );
+  });
+
+  app.post("/transaction_all",(req,res) =>{
+    trans = db.get_transactions(req.body)
+    if(trans){
+      res.render("view_transactions",{transaction:trans})
+    }
+    else{
+      console.log(trans)
+      console.log("No data found")
+      res.render("view_transactions",{transaction:null})
+    }
+
+  })
 app.post("/insert_product", (req, res) => {
-    grp = db.get_groups()
-    console.log((req.body))
     s = db.insert_products(req.body)
     if (s) {
         res.render("insert_product", { groups: grp, inserted: true })
@@ -50,32 +90,222 @@ app.post("/insert_product", (req, res) => {
 })
 
 app.post("/insert_creditorProfile", (req, res) => {
-    console.log(req.body);
+
+    console.log(req.body)
+ 
     s = db.insert_creditorProfile(req.body);
+    if (s) {
+        res.render("insert_creditorProfile", { inserted: true })
+    }
+    else {
+        res.render("insert_creditorProfile", {inserted: false })
+    }
     console.log(s);
+  });
+
+
+  app.post("/creditors_update", (req, res) => {
+
+    console.log(req.body)
+ 
+    s = db.update_creditorProfile(req.body);
+    if (s) {
+      creds = db.get_creditors()
+        res.render("view_creditors", { creditors: creds })
+    }
+    else {
+        res.render("view_creditors", {creditors: null })
+    }
+    console.log(s);
+    
+  });
+
+  app.post("/product_update", (req, res) => {
+
+    console.log(req.body)
+ 
+    s = db.update_product(req.body);
+    if (s) {
+
+      prods = db.get_products();
+  res.render("view_product",{product: prods,del:null,update:true});
+    
+    }
+    else {
+      prods = db.get_products();
+      res.render("view_product",{product: prods,del:null,update:false});
+    }
+    console.log(s);
+    
+  });
+  app.post("/supplier_update",(req,res) => {
+    console.log(req.body)
+    s = db.update_supplier(req.body)
+    sup = db.get_suppilers()
+  console.log("getting supplier")
+  console.log(sup)
+  if(s){
+    res.render("view_supplier",{suppliers:sup,del:null,update:true})
+  }
+  else{
+  res.render("view_supplier",{suppliers:sup,del:null,update:false})
+  }
+
   });
   
   app.post("/insert_supplier", (req, res) => {
     console.log(req.body);
     s = db.insert_supplier(req.body);
+    if (s) {
+        res.render("insert_supplier", { inserted: true })
+    }
+    else {
+        res.render("insert_supplier", { inserted: false })
+    }
     console.log(s);
   });
   
   app.post("/insert_retailer", (req, res) => {
     console.log(req.body);
     s = db.insert_retailer(req.body);
+    if (s) {
+        res.render("login",{log : null})
+    }
+    else {
+        res.render("login",{log : null})
+    }
     console.log(s);
   });
 
   app.post("/insert_productGroup", (req, res) => {
     console.log(req.body);
     s = db.insert_productGroup(req.body);
+    if (s) {
+        res.render("insert_productGroup", { inserted: true })
+    }
+    else {
+        res.render("insert_productGroup", { inserted: false })
+    }
     console.log(s);
   });  
+  app.post("/login_retailer",(req, res) =>{
+    console.log(req.body)
+    s = db.get_retailer(req.body);
+    if(s){
+      res.render("index")
+    }
+    else{
+      res.render("login",{log:false})
+    }
+
+  })
 app.get("/place_order", (req, res) => {
     s = db.get_suppilers()
     res.render("place_order", { suppliers })
 })
+
+app.get("/del_creditor", (req, res) => {
+  console.log("inside del 1app");
+  console.log(req.query);
+  console.log("inside de00l app");
+  console.log(req.query.id);
+
+  console.log(req.body);
+  s = db.del_creditor(req.query);
+  if(s){
+    creds = db.get_creditors();
+  res.render("view_creditors",{creditors:creds});
+}
+else{
+  res.render("view_creditors",{creditors:null});
+}
+
+})
+
+app.get("/del_product", (req, res) => {
+  console.log("inside del product");
+  console.log(req.query);
+  console.log("inside del product");
+  console.log(req.query.id);
+
+  console.log(req.body);
+  s = db.del_product(req.query);
+  if(s){
+    prods = db.get_products();
+  res.render("view_product",{product: prods,del:true , update:null});
+}
+else{
+  prods = db.get_products();
+  res.render("view_product",{product:prods,del:false,update:null});
+}
+
+});
+
+app.get("/del_supplier", (req, res) => {
+  console.log("inside del product");
+  console.log(req.query);
+  console.log("inside del product");
+  console.log(req.query.id);
+
+  console.log(req.body);
+  s = db.del_supplier(req.query);
+  sup = db.get_suppilers()
+  console.log("getting supplier")
+  console.log(sup)
+  if(s){
+    res.render("view_supplier",{suppliers:sup,del:true,update:null})
+}
+else{
+  res.render("view_supplier",{suppliers:sup,del:false,update:null})
+}
+
+});
+
+app.get("/update_creditor",(req,res) =>{
+  console.log(req.query);
+  creds = db.get_creditors_id(req.query)
+  console.log(creds)
+  console.log("id form update ")
+  if(creds){
+    res.render("update_CreditorProfile",{creditors:creds});
+  }else{
+    res.render("update_CreditorProfile",{creditors:null});
+  }
+
+
+} )
+
+app.get("/login",(req,res) =>{
+  res.render("login")
+})
+app.get("/update_supplier",(req,res)=>{
+  console.log(req.query);
+  sup = db.get_supplier_by_id(req.query)
+  console.log(sup)
+  console.log("id form update ")
+  if(sup){
+    res.render("update_Supplier",{suppliers:sup});
+  }else{
+    res.render("update_Supplier",{suppliers:null});
+  }
+
+}
+)
+app.get("/update_product",(req,res) =>{
+  console.log(req.query);
+   prods= db.get_product_by_id(req.query)
+  console.log(prods)
+  console.log(" product id form update ")
+  grp = db.get_groups()
+    console.log(grp)
+  if(prods){
+    res.render("update_Product",{product:prods,groups:grp });
+  }else{
+    res.render("update_Product",{product:null,groups:null });
+  }
+
+
+} )
 
 app.listen(3000, () => {
     console.log("server started on port 3000");
