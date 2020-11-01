f806ce10332a3589c4b4e59d2dc688bb348ef38f
shravani20git <chavanke4shravani@gmail.com>
2020-11-01 18:02:07 +0530

    loggin changed

--- index.js
+++ index.js
@@ -14,68 +14,141 @@ app.use( express.static( "images" ) );
 
 var order_session = null
 var credit_session = null
+var log = null
 app.get("/", (req, res) => {
     res.render("login",{log:null})
 })
 
 
 app.get("/index", (req, res) => {
+  if (log == null){
+    res.render("login",{log:null})
+
+  }
+  else{
   res.render("index")
+  }
 })
 app.get("/view_creditors", (req, res) => {
+  if (log == null){
+    res.render("login",{log:null})
+
+  }
+  else{
   creds = db.get_creditors()
   console.log(creds)
   res.render("view_creditors",{creditors:creds})
+  }
 })
 
 app.get("/view_currentInvent", (req, res) => {
+  if (log == null){
+    res.render("login",{log:null})
+
+  }
+  else{
   invt = db.get_currentInventory()
   console.log(invt)
   res.render("view_currentInvent",{current:invt})
+  }
 })
 app.get("/view_product", (req, res) => {
+  if (log == null){
+    res.render("login",{log:null})
+
+  }
+  else{
   prods = db.get_product()
   console.log("getting products")
   console.log(prods)
   res.render("view_product",{product:prods,del:null,update:null})
+  }
 })
 
 
 app.get("/view_supplier", (req, res) => {
+  if (log == null){
+    res.render("login",{log:null})
+
+  }
+  else{
   sup = db.get_suppilers()
   console.log("getting supplier")
   console.log(sup)
   res.render("view_supplier",{suppliers:sup,del:null,update:null})
+  }
 })
 app.get("/insert_product", (req, res) => {
+  if (log == null){
+    res.render("login",{log:null})
+
+  }
+  else{
     grp = db.get_groups()
     console.log(grp)
     res.render("insert_product", { groups: grp, inserted: null })
+  }
 })
 app.get("/insert_creditorProfile", (req, res) => {
+  if (log == null){
+    res.render("login",{log:null})
+
+  }
+  else{
 
     res.render("insert_creditorProfile", {inserted: null});
+  }
   });
   app.get("/insert_supplier", (req, res) => {
+    if (log == null){
+      res.render("login",{log:null})
+  
+    }
+    else{
+
     res.render("insert_supplier", {inserted: null});
+    }
   });
   app.get("/insert_retailer", (req, res) => {
+    if (log == null){
+      res.render("login",{log:null})
+  
+    }
+    else{
     res.render("insert_retailer", {inserted: null});
+    }
   });
   
   app.get("/insert_productGroup", (req, res) => {
+    if (log == null){
+      res.render("login",{log:null})
+  
+    }
+    else{
   
     res.render("insert_productGroup", {inserted: null});
+    }
   });
 
   app.get("/view_transactions", (req, res) => {
    // trans = db.get_transactions()
     //console.log(trans)
+    if (log == null){
+      res.render("login",{log:null})
+  
+    }
+    else{
   
     res.render("view_trans", {transaction: null} );
+    }
   });
 
   app.post("/transaction_all",(req,res) =>{
+    if (log == null){
+      res.render("login",{log:null})
+  
+    }
+    else{
     trans = db.get_transactions(req.body)
     if(trans){
       res.render("view_transactions",{transaction:trans})
@@ -85,9 +158,14 @@ app.get("/insert_creditorProfile", (req, res) => {
       console.log("No data found")
       res.render("view_transactions",{transaction:null})
     }
-
-  })
+  }
+  });
 app.post("/insert_product", (req, res) => {
+  if (log == null){
+    res.render("login",{log:null})
+
+  }
+  else{
     s = db.insert_products(req.body)
     if (s) {
         res.render("insert_product", { groups: grp, inserted: true })
@@ -96,9 +174,15 @@ app.post("/insert_product", (req, res) => {
         res.render("insert_product", { groups: grp, inserted: false })
     }
     console.log(s)
+  }
 })
 
 app.post("/insert_creditorProfile", (req, res) => {
+  if (log == null){
+    res.render("login",{log:null})
+
+  }
+  else{
 
     console.log(req.body)
  
@@ -110,10 +194,16 @@ app.post("/insert_creditorProfile", (req, res) => {
         res.render("insert_creditorProfile", {inserted: false })
     }
     console.log(s);
+  }
   });
 
 
   app.post("/creditors_update", (req, res) => {
+    if (log == null){
+      res.render("login",{log:null})
+  
+    }
+    else{
 
     console.log(req.body)
  
@@ -126,10 +216,15 @@ app.post("/insert_creditorProfile", (req, res) => {
         res.render("view_creditors", {creditors: null })
     }
     console.log(s);
-    
+  }
   });
 
   app.post("/product_update", (req, res) => {
+    if (log == null){
+      res.render("login",{log:null})
+  
+    }
+    else{
 
     console.log(req.body)
  
@@ -145,9 +240,14 @@ app.post("/insert_creditorProfile", (req, res) => {
       res.render("view_product",{product: prods,del:null,update:false});
     }
     console.log(s);
-    
+  }
   });
   app.post("/supplier_update",(req,res) => {
+    if (log == null){
+      res.render("login",{log:null})
+  
+    }
+    else{
     console.log(req.body)
     s = db.update_supplier(req.body)
     sup = db.get_suppilers()
@@ -159,10 +259,15 @@ app.post("/insert_creditorProfile", (req, res) => {
   else{
   res.render("view_supplier",{suppliers:sup,del:null,update:false})
   }
-
+    }
   });
   
   app.post("/insert_supplier", (req, res) => {
+    if (log == null){
+      res.render("login",{log:null})
+  
+    }
+    else{
     console.log(req.body);
     s = db.insert_supplier(req.body);
     if (s) {
@@ -172,9 +277,15 @@ app.post("/insert_creditorProfile", (req, res) => {
         res.render("insert_supplier", { inserted: false })
     }
     console.log(s);
+  }
   });
   
   app.post("/insert_retailer", (req, res) => {
+    if (log == null){
+      res.render("login",{log:null})
+  
+    }
+    else{
     console.log(req.body);
     s = db.insert_retailer(req.body);
     if (s) {
@@ -184,9 +295,15 @@ app.post("/insert_creditorProfile", (req, res) => {
         res.render("login",{log : null})
     }
     console.log(s);
+  }
   });
 
   app.post("/insert_productGroup", (req, res) => {
+    if (log == null){
+      res.render("login",{log:null})
+  
+    }
+    else{
     console.log(req.body);
     s = db.insert_productGroup(req.body);
     if (s) {
@@ -196,11 +313,14 @@ app.post("/insert_creditorProfile", (req, res) => {
         res.render("insert_productGroup", { inserted: false })
     }
     console.log(s);
+  }
   });  
   app.post("/login_retailer",(req, res) =>{
+    
     console.log(req.body)
     s = db.get_retailer(req.body);
     if(s){
+      log = true
       res.render("index")
     }
     else{
@@ -209,35 +329,70 @@ app.post("/insert_creditorProfile", (req, res) => {
 
   })
 app.get("/place_order1", (req, res) => {
+  if (log == null){
+    res.render("login",{log:null})
+
+  }
+  else{
     s = db.get_suppilers()
     //p = db.get_products()
     res.render("place_order1", { suppliers: s })
+  }
 })
 app.get("/place_order2/:id", (req, res) => {
+  if (log == null){
+    res.render("login",{log:null})
+
+  }
+  else{
     let sup_id = req.params.id
     order_session = { supplier_id: sup_id }
     p = db.get_products()
     res.render("place_order2", { supplier_id: sup_id, products: p })
+  }
 })
 
 app.post("/place_order3", (req, res) => {
+  if (log == null){
+    res.render("login",{log:null})
+
+  }
+  else{
     console.log(JSON.parse(req.body.products))
     order_session.products = JSON.parse(req.body.products)
     res.json(order_session)
+  }
 })
 
 app.get("/place_order_final", (req, res) => {
+  if (log == null){
+    res.render("login",{log:null})
+
+  }
+  else{
     console.log(order_session)
     let r = db.insert_order(order_session)
     res.render("confirmed_order", { placed: r })
+  }
 })
 
 app.get("/order_status", (req, res) => {
+  if (log == null){
+    res.render("login",{log:null})
+
+  }
+  else{
     od = db.get_order_details()
     res.render("order_status", { order_details: od })
+  }
 })
 
 app.post("/update_order", (req, res) => {
+  if (log == null){
+    res.render("login",{log:null})
+
+  }
+  else{
     console.log(req.body)
     u = db.update_order(req.body)
     if (u) {
@@ -246,27 +401,51 @@ app.post("/update_order", (req, res) => {
     else {
         res.json({ done: false })
     }
+  }
 })
 
 app.get("/credit_products1", (req, res) => {
+  if (log == null){
+    res.render("login",{log:null})
+
+  }
+  else{
     c = db.get_creditors()
     //p = db.get_products()
     res.render("credit_products1", { creditors: c })
+  }
 })
 
 app.get("/credit_products2/:id", (req, res) => {
+  if (log == null){
+    res.render("login",{log:null})
+
+  }
+  else{
     let cred_id = req.params.id
     credit_session = { creditor_id: cred_id }
     p = db.get_products()
     res.render("credit_products2", { creditor_id: cred_id, products: p })
+  }
 })
 app.post("/credit_products3", (req, res) => {
+  if (log == null){
+    res.render("login",{log:null})
+
+  }
+  else{
     console.log(JSON.parse(req.body.products))
     credit_session.products = JSON.parse(req.body.products)
     res.json(credit_session)
+  }
 })
 
 app.get("/del_creditor", (req, res) => {
+  if (log == null){
+    res.render("login",{log:null})
+
+  }
+  else{
   console.log("inside del 1app");
   console.log(req.query);
   console.log("inside de00l app");
@@ -281,10 +460,16 @@ app.get("/del_creditor", (req, res) => {
 else{
   res.render("view_creditors",{creditors:null});
 }
+}
 
 })
 
 app.get("/del_product", (req, res) => {
+  if (log == null){
+    res.render("login",{log:null})
+
+  }
+  else{
   console.log("inside del product");
   console.log(req.query);
   console.log("inside del product");
@@ -300,10 +485,16 @@ else{
   prods = db.get_product();
   res.render("view_product",{product:prods,del:false,update:null});
 }
+  }
 
 });
 
 app.get("/del_supplier", (req, res) => {
+  if (log == null){
+    res.render("login",{log:null})
+
+  }
+  else{
   console.log("inside del product");
   console.log(req.query);
   console.log("inside del product");
@@ -320,10 +511,16 @@ app.get("/del_supplier", (req, res) => {
 else{
   res.render("view_supplier",{suppliers:sup,del:false,update:null})
 }
+  }
 
 });
 
 app.get("/update_creditor",(req,res) =>{
+  if (log == null){
+    res.render("login",{log:null})
+
+  }
+  else{
   console.log(req.query);
   creds = db.get_creditors_id(req.query)
   console.log(creds)
@@ -334,13 +531,19 @@ app.get("/update_creditor",(req,res) =>{
     res.render("update_CreditorProfile",{creditors:null});
   }
 
-
+  }
 } )
 
 app.get("/login",(req,res) =>{
+  log = null
   res.render("login",{log : null})
 })
 app.get("/update_supplier",(req,res)=>{
+  if (log == null){
+    res.render("login",{log:null})
+
+  }
+  else{
   console.log(req.query);
   sup = db.get_supplier_by_id(req.query)
   console.log(sup)
@@ -352,8 +555,14 @@ app.get("/update_supplier",(req,res)=>{
   }
 
 }
-)
+}
+);
 app.get("/update_product",(req,res) =>{
+  if (log == null){
+    res.render("login",{log:null})
+
+  }
+  else{
   console.log(req.query);
    prods= db.get_product_byId(req.query)
   console.log(prods)
@@ -365,29 +574,52 @@ app.get("/update_product",(req,res) =>{
   }else{
     res.render("update_Product",{product:null,groups:null });
   }
-
+  }
 
 } )
 
 
 app.post("/place_order3", (req, res) => {
+  if (log == null){
+    res.render("login",{log:null})
+
+  }
+  else{
     console.log(JSON.parse(req.body.products))
     order_session.products = JSON.parse(req.body.products)
     res.json(order_session)
+  }
 })
 
 app.get("/place_order_final", (req, res) => {
+  if (log == null){
+    res.render("login",{log:null})
+
+  }
+  else{
     console.log(order_session)
     let r = db.insert_order(order_session)
     res.render("confirmed_order", { placed: r })
+  }
 })
 
 app.get("/order_status", (req, res) => {
+  if (log == null){
+    res.render("login",{log:null})
+
+  }
+  else{
     od = db.get_order_details()
     res.render("order_status", { order_details: od })
+  }
 })
 
 app.post("/update_order", (req, res) => {
+  if (log == null){
+    res.render("login",{log:null})
+
+  }
+  else{
     console.log(req.body)
     u = db.update_order(req.body)
     if (u) {
@@ -396,39 +628,75 @@ app.post("/update_order", (req, res) => {
     else {
         res.json({ done: false })
     }
+  }
 })
 
 app.get("/credit_products1", (req, res) => {
+  if (log == null){
+    res.render("login",{log:null})
+
+  }
+  else{
     c = db.get_creditors()
     //p = db.get_products()
     res.render("credit_products1", { creditors: c })
+  }
 })
 
 app.get("/credit_products2/:id", (req, res) => {
+  if (log == null){
+    res.render("login",{log:null})
+
+  }
+  else{
     let cred_id = req.params.id
     credit_session = { creditor_id: cred_id }
     p = db.get_products()
     res.render("credit_products2", { creditor_id: cred_id, products: p })
+  }
 })
 app.post("/credit_products3", (req, res) => {
+  if (log == null){
+    res.render("login",{log:null})
+
+  }
+  else{
     console.log(JSON.parse(req.body.products))
     credit_session.products = JSON.parse(req.body.products)
     res.json(credit_session)
+  }
 })
 
 
 app.get("/credit_products_final", (req, res) => {
+  if (log == null){
+    res.render("login",{log:null})
+
+  }
+  else{
     console.log(credit_session)
     let r = db.credit_products(credit_session)
     res.render("confirmed_credit", { placed: r })
+  }
 })
 
 app.get("/creditor_status", (req, res) => {
+  if (log == null){
+    res.render("login",{log:null})
+
+  }
+  else{
     cd = db.get_creditors()
     res.render("creditor_status", { creditor_details: cd })
+  }
 })
 
 app.post("/update_creditor", (req, res) => {
+  if (log == null){
+    res.render("login",{log:null})
+
+  }
+  else{
     console.log(req.body)
     u = db.update_creditor_paid(req.body)
     if (u) {
@@ -437,14 +705,26 @@ app.post("/update_creditor", (req, res) => {
     else {
         res.json({ done: false })
     }
+  }
 })
 
 app.get("/quick_sell", (req, res) => {
+  if (log == null){
+    res.render("login",{log:null})
+
+  }
+  else{
     prod = db.get_products()
     res.render("quick_sell", { products: prod, sold: null })
+  }
 })
 
 app.post("/quick_sell", (req, res) => {
+  if (log == null){
+    res.render("login",{log:null})
+
+  }
+  else{
     console.log(req.body)
     s = db.quick_sell(req.body)
     prod = db.get_products()
@@ -453,7 +733,8 @@ app.post("/quick_sell", (req, res) => {
     }
     else {
         res.render("quick_sell", { products: prod, sold: false })
-    }
+    }}
+
 })
 app.listen(3000, () => {
     console.log("server started on port 3000");
